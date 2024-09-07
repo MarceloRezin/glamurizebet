263cffd6e6d16a0f47b607e437a5337f46052417
Marcelo Rezin <marcelo.rezin@gmail.com>
2024-09-07 17:37:41 -0300

    Carrega as configurações do env e coloca no contexto a conexão com o banco

--- back/cmd/glamurizebet/main.go
+++ back/cmd/glamurizebet/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	appcontext "glamurizebet/internal/application"
+	"glamurizebet/internal/db"
 	"glamurizebet/internal/endpoints"
 	endpoint_util "glamurizebet/internal/endpoints/util"
 
@@ -8,21 +10,37 @@ import (
 )
 
 func main() {
+
+	env, err := appcontext.LoadEnv()
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := db.Create(&env)
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	ac := appcontext.AppContext{
+		DB: db,
+	}
+
 	router := gin.Default()
 
 	v1 := router.Group("/api/v1")
 	{
 		external := v1.Group("/external")
 		{
-			endpoints.AddRoutesBy(endpoint_util.V1, endpoint_util.EXTERNAL, external)
+			endpoints.AddRoutesBy(endpoint_util.V1, endpoint_util.EXTERNAL, external, &ac)
 		}
 
 		play := v1.Group("/play")
 		{
-			endpoints.AddRoutesBy(endpoint_util.V1, endpoint_util.PLAY, play)
+			endpoints.AddRoutesBy(endpoint_util.V1, endpoint_util.PLAY, play, &ac)
 		}
 
-		endpoints.AddRoutesBy(endpoint_util.V1, endpoint_util.COMMON, v1)
+		endpoints.AddRoutesBy(endpoint_util.V1, endpoint_util.COMMON, v1, &ac)
 	}
 
 	router.Run("localhost:8000")
