1307ad7ed8e2ec823d738773a28150b3816c5c80
Marcelo Rezin <marcelo.rezin@gmail.com>
2024-09-22 19:32:41 -0300

    Criação do contexto da aplicação | inicialização do banco | tratamento de erros | log de informações | configuração do gin caso seja prod

--- back/cmd/glamurizebet/main.go
+++ back/cmd/glamurizebet/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	appcontext "glamurizebet/internal/application"
-	"glamurizebet/internal/db"
 	"glamurizebet/internal/endpoints"
 	endpoint_util "glamurizebet/internal/endpoints/util"
 
@@ -16,32 +15,41 @@ func main() {
 		panic(err)
 	}
 
-	db, err := db.Create(&env)
-
+	appcontext, err := appcontext.GetAppContext(&env)
 	if err != nil {
-		panic("failed to connect database")
+		panic(err)
 	}
 
-	ac := appcontext.AppContext{
-		DB: db,
+	appcontext.Log.Info.Println("configuring GIN mode")
+	if appcontext.IsProd {
+		gin.SetMode(gin.ReleaseMode)
+		gin.DisableConsoleColor()
+		gin.DefaultWriter = appcontext.Log.Info.Writer()
+
+		appcontext.Log.Info.Println("gin setted to release mode")
+	} else {
+		gin.SetMode(gin.DebugMode)
+		appcontext.Log.Info.Println("gin setted to debug mode")
 	}
 
 	router := gin.Default()
 
+	appcontext.Log.Info.Println("creating routes")
 	v1 := router.Group("/api/v1")
 	{
 		external := v1.Group("/external")
 		{
-			endpoints.AddRoutesBy(endpoint_util.V1, endpoint_util.EXTERNAL, external, &ac)
+			endpoints.AddRoutesBy(endpoint_util.V1, endpoint_util.EXTERNAL, external, &appcontext)
 		}
 
 		play := v1.Group("/play")
 		{
-			endpoints.AddRoutesBy(endpoint_util.V1, endpoint_util.PLAY, play, &ac)
+			endpoints.AddRoutesBy(endpoint_util.V1, endpoint_util.PLAY, play, &appcontext)
 		}
 
-		endpoints.AddRoutesBy(endpoint_util.V1, endpoint_util.COMMON, v1, &ac)
+		endpoints.AddRoutesBy(endpoint_util.V1, endpoint_util.COMMON, v1, &appcontext)
 	}
 
+	appcontext.Log.Info.Println("starting server")
 	router.Run("localhost:8000")
 }
--- back/internal/application/app_context.go
+++ back/internal/application/app_context.go
@@ -1,7 +1,52 @@
 package application
 
-import "gorm.io/gorm"
+import (
+	"errors"
+	"fmt"
+
+	golog "github.com/MarceloRezin/go-log/cmd/go-log"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 type AppContext struct {
-	DB *gorm.DB
+	Log    *golog.Logger
+	DB     *gorm.DB
+	IsProd bool
+}
+
+func GetAppContext(env *Env) (AppContext, error) {
+	appContext := AppContext{
+		IsProd: env.IsProd,
+	}
+
+	var err error
+	if env.IsProd {
+		appContext.Log, err = golog.DefaultFile()
+	} else {
+		appContext.Log, err = golog.DefaultConsole()
+	}
+
+	if err != nil {
+		return appContext, errors.New("it was not possible get logger")
+	}
+
+	appContext.Log.Info.Println("logger started")
+
+	db, err := createDbConnection(env)
+	if err != nil {
+		return appContext, errors.New("it was not possible get db connection")
+	}
+
+	appContext.Log.Info.Println("database started")
+
+	appContext.DB = db
+
+	return appContext, nil
+}
+
+func createDbConnection(env *Env) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(144.22.223.201:1432)/%s?charset=utf8mb4&parseTime=True&loc=Local", env.DbUser, env.DbPass, env.DbName)
+
+	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
 }
